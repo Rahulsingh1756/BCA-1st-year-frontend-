1bf6e1f9c29a1267051079fd1b4cba3bd67a0969
Your Name <you@example.com>
2024-10-17 10:15:55 +0530

    first commit

---
+++ day 1/day 2/day 3/index.js
@@ -0,0 +1,22 @@
+
+let v = 5
+v= 8
+console.log(v)
+const k = 10
+console.log(k)
+
+
+let hey = 40
+if(hey===40) {
+console.log("my value is", hey)
+}else if(v===8){
+    console.log("my value is", v);
+}
+
+for(let i=0;i<5;i++){
+    console.log("i=", i)
+}
+
+
+const arr=[0,"bca 2nd year", "apple", true,false];
+console.log(typeof(array));
